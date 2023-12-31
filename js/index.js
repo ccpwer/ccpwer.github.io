7a740f787c165a8baa2435941ef66116a3e2aba9
ccpwer <2435565917@qq.com>
2023-12-31 23:48:32 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||2024|谢楚邻||新年快乐||I Love U||#rectangle|');
+      S.UI.simulate('|#countdown 3||2024|谢楚邻|新年快乐|I Love U||#rectangle|');
     }
 
     S.Drawing.loop(function () {
